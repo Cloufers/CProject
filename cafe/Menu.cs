97fa9147532d48051ae0b571957bd45763bd97cb
Вультурьйол <@DESKTOP-VGB1S4O>
2025-02-06 02:10:52 +0300

    Ресы

--- cafe/Menu.cs
+++ cafe/Menu.cs
@@ -20,13 +20,14 @@
             form2.Show();
             Hide();
         }
+
         private void adminMenuButton_Click(object sender, EventArgs e)
         {
             using (PasswordForm passwordForm = new PasswordForm())
             {
                 if (passwordForm.ShowDialog() == DialogResult.OK)
                 {
-                    string username = passwordForm.EnteredUsername; 
+                    string username = passwordForm.EnteredUsername;
                     string enteredPassword = passwordForm.EnteredPassword;
                     string connectionString = "Host=ep-fragrant-shadow-a2mncv4x-pooler.eu-central-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_DXn7qEayl1Yz";
 
@@ -43,7 +44,7 @@
                 }
             }
         }
-       
+
 
     }
 }
