143bfcf6f04a1995723ee82acbb4067643d546a3
Cloufers <69718347+Cloufers@users.noreply.github.com>
2025-02-06 01:51:22 +0300

    add: added FeedbackForm

---
+++ cafe/Feedback1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace cafe
+{
+    partial class Feedback1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            pbStars = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pbStars).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(110, 381);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(517, 239);
+            textBox1.TabIndex = 0;
+            // 
+            // pbStars
+            // 
+            pbStars.Image = Properties.Resources.StarEmpty;
+            pbStars.Location = new Point(36, 111);
+            pbStars.Name = "pbStars";
+            pbStars.Size = new Size(663, 157);
+            pbStars.SizeMode = PictureBoxSizeMode.Zoom;
+            pbStars.TabIndex = 1;
+            pbStars.TabStop = false;
+            pbStars.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(288, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Поставьте нам оценку";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(290, 358);
+            label2.Name = "label2";
+            label2.Size = new Size(163, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Напишите свой отзыв";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(293, 626);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 30);
+            button1.TabIndex = 4;
+            button1.Text = "Отправить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Feedback1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(732, 736);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pbStars);
+            Controls.Add(textBox1);
+            Name = "Feedback1";
+            Text = "Feedback1";
+            ((System.ComponentModel.ISupportInitialize)pbStars).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private PictureBox pbStars;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+    }
+}
---
+++ cafe/Feedback1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cafe
+{
+    public partial class Feedback1 : Form
+    {
+        public Feedback1()
+        {
+            InitializeComponent();
+            InitializeStarPictureBox();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { }
+
+
+
+
+        private void pbStars_Click(object sender, EventArgs e)
+        { }
+        private const int StarCount = 5;
+        private int selectedStars = 0;
+
+        private void InitializeStarPictureBox()
+        {
+            pbStars.Image = CreateStarImage(0);
+            pbStars.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbStars.Click += PbStars_Click;
+        }
+
+        private void PbStars_Click(object sender, EventArgs e)
+        {
+            // Определяем, сколько звезд выбрано
+            Point mousePosition = pbStars.PointToClient(Cursor.Position);
+            int starWidth = pbStars.Width / StarCount;
+
+            selectedStars = mousePosition.X / starWidth + 1;
+            if (selectedStars > StarCount) selectedStars = StarCount;
+
+            pbStars.Image = CreateStarImage(selectedStars);
+        }
+
+        private Image CreateStarImage(int filledStars)
+        {
+            Bitmap bitmap = new Bitmap(pbStars.Width, pbStars.Height);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                for (int i = 0; i < StarCount; i++)
+                {
+                    Image starImage = (i < filledStars) ? Properties.Resources.StarFilled : Properties.Resources.StarEmpty;
+                    g.DrawImage(starImage, i * (bitmap.Width / StarCount), 0, bitmap.Width / StarCount, bitmap.Height);
+                }
+            }
+            return bitmap;
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string review = textBox1.Text;
+            MessageBox.Show($"Отзыв: {review}\nОценка: {selectedStars} ", "Отправлено");
+        }
+    }
+}
+
+    
+
